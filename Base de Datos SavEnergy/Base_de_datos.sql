60bf9fc06ac092569fe29278cc6bf3bca1687462
Miguel Montiel <31743982+miguelmontiel30@users.noreply.github.com>
2018-04-15 08:31:12 -0500

    Update Base_de_datos.sql
    - Cambio Final de la BD

--- Base de Datos SavEnergy/Base_de_datos.sql
+++ Base de Datos SavEnergy/Base_de_datos.sql
@@ -20,8 +20,9 @@ cuota VARCHAR(10) NOT NULL);
 
 CREATE TABLE rangos_consumo(
 id_rango INT(4) ZEROFILL AUTO_INCREMENT NOT NULL,
-rango INT(4) NOT NULL,
-precio FLOAT(4,4) NOT NULL,
+rango_minimo INT(4) NOT NULL,
+rango_maximo INT(4) NOT NULL,
+precio FLOAT(3) NOT NULL,
 id_nivel_consumo INT(2) ZEROFILL NOT NULL,
 id_cuotas INT(2) ZEROFILL NOT NULL,
 id_tarifa INT(2) ZEROFILL NOT NULL,
@@ -36,43 +37,36 @@ foto MEDIUMBLOB NULL,
 contrasenia varchar(15) not null,
 email varchar(25) NOT NULL,
 nombre VARCHAR(25) NULL,
-fecha_de_corte date NULL,
-id_tarifa INT(2) ZEROFILL NULL,
-id_cuotas INT(2) ZEROFILL NULL,
+id_tarifa INT(2) ZEROFILL NOT NULL,
+id_cuotas INT(2) ZEROFILL NOT NULL,
+id_clave_producto VARCHAR(15) NOT NULL,
 tipo_usuario varchar(10) NOT NULL,
+foreign key (id_clave_producto) references productos(id_clave_producto),
 foreign key (id_cuotas) references cuotas(id_cuotas),
 foreign key (id_tarifa) references tarifas(id_tarifa));
 
 CREATE TABLE productos(
-id_producto INT(8) ZEROFILL AUTO_INCREMENT NOT NULL PRIMARY KEY,
-id_usuario INT(8) ZEROFILL NULL,
-clave_producto VARCHAR(20) NOT NULL,
+id_clave_producto VARCHAR(15) NOT NULL PRIMARY KEY,
 estado VARCHAR(10) NULL,
-descripcion VARCHAR(50) NULL,
-id_tarifa INT(2) ZEROFILL NULL,
-id_cuotas INT(2) ZEROFILL NULL,
-foreign key (id_cuotas) references cuotas(id_cuotas),
-foreign key (id_tarifa) references tarifas(id_tarifa),
-foreign key (id_usuario) references usuarios(id_usuario));
+descripcion VARCHAR(50) NULL);
 
 CREATE TABLE periodos_corte(
 id_periodo INT(8) ZEROFILL AUTO_INCREMENT NOT NULL PRIMARY KEY,
-id_tipo_consumo INT(2) ZEROFILL NULL,
-fecha_corte_inicio DATE NOT NULL,
-fecha_corte_final DATE NOT NULL,
-foreign key (id_tipo_consumo) references tipo_consumo(id_tipo_consumo));
+id_usuario INT(8) ZEROFILL NOT NULL,
+fecha_inicio DATE NOT NULL,
+fecha_corte DATE NOT NULL,
+lectura INT(6) NOT NULL,
+foreign key (id_clave_producto) references productos(id_clave_producto));
 
 CREATE TABLE consumos(
 id_usuario INT(8) ZEROFILL NOT NULL,
 fecha DATE NOT NULL,
 volts FLOAT(4,4) NOT NULL,
 id_tipo_consumo INT(2) ZEROFILL NOT NULL,
-id_periodo INT(8) ZEROFILL NOT NULL,
-id_rango INT(4) ZEROFILL NOT NULL,
+id_rango INT(4) ZEROFILL NULL,
+FOREIGN KEY (id_rango) references rangos_consumo(id_rango),
 FOREIGN KEY (id_usuario) references usuarios(id_usuario),
-FOREIGN KEY (id_tipo_consumo) references tipo_consumo(id_tipo_consumo),
-FOREIGN KEY (id_periodo) references periodos_corte(id_periodo),
-FOREIGN KEY (id_rango) references rangos_consumo(id_rango));
+FOREIGN KEY (id_tipo_consumo) references tipo_consumo(id_tipo_consumo));
 
 
 
